d8227abc9f13fb3827e1582ac44a5fb1af9329b2
romacas01 <romacas01@gmail.com>
2026-05-30 21:39:34 +0100

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprof = 0;
+
+        for (int i = 0; i < prices.length -1; i++){
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] > prices[i]) {
+                    int profit = prices[j] - prices[i];
+                    maxprof = Math.max(maxprof,profit);
+                }
+            }
+        }
+
+        return maxprof;
+    }
+}
